b9dabe079a998d1930eca0fb20860517dfee64ff
adebowale.odulaja <brownwoodinc@hotmail.com>
2022-02-28 22:22:48 +0000

    code for div

--- alertfunction.js
+++ alertfunction.js
@@ -2,16 +2,26 @@ function clickFunc() {
     var element = document.getElementById('txtInput');
     if(element.value == ""){
         alert('Please fill the text filed.');
-        return;    
+        return;
     }
     alert(element.value);
     element.value = '';
-    //variable_name_to_set_val_for_div.firstChild.nodeValue = element.value;
+}
+
+function divFunc() {
+    var textElement = document.getElementById('txtInput');
+    var divElement = document.getElementById('myDiv');
+    if(textElement.value == ""){
+        alert('Please fill the text filed.');
+        return;
+    }
+    divElement.firstChild.nodeValue = textElement.value;
+    textElement.value = '';
 }
 
 function myLoadFunction() {
     var element = document.getElementById('button');
-    element.addEventListener('click', clickFunc);
+    element.addEventListener('click', divFunc);
 }
 
 document.addEventListener('DOMContentLoaded', myLoadFunction);
